7c3aaa5230e83225b453a30938631b9e8f6c480d
Jy Kingston <jy.kingston@gmail.com>
2022-09-19 12:15:48 +1000

    wip

---
+++ main.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  default = "82323"
+  type    = string
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    TicketNumber = "ticket82323"
+    OwnedBy      = "jy.kingston@hashicorp.com"
+    CreatorName  = "Jy Kingston"
+    HashiRole    = "Terraform Sr Support Engineer"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Region to APSE2
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+#=============================================
+
+resource "tls_private_key" "bitbucket_access" {
+  algorithm = "RSA"
+}
